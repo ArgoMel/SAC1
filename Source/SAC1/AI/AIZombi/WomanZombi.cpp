32335bb00fa19ff55cc0fdc05c22ff54dd9cb29a
WI-Jeong <cpdls23@naver.com>
2023-10-06 20:23:45 +0900

    좀비 메쉬 5개 추가 & 우먼좀비 기본 c++ 추가
    좀비 메쉬 5개 추가 & 우먼좀비 기본 c++ 추가

---
+++ Source/SAC1/AI/AIZombi/WomanZombi.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "WomanZombi.h"
+
+AWomanZombi::AWomanZombi()
+{
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh>	MeshAsset(TEXT("/Script/Engine.SkeletalMesh'/Game/Zombi/WomanZombi/Zombiegirl_W_Kurniawan.Zombiegirl_W_Kurniawan'"));
+
+	if (MeshAsset.Succeeded())
+		mMesh->SetSkeletalMesh(MeshAsset.Object);
+}
+
+void AWomanZombi::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AWomanZombi::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+}
+
+void AWomanZombi::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
