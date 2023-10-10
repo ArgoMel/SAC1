01551144c32bb99775c409d7eb924e73ff74a17e
WI-Jeong <cpdls23@naver.com>
2023-10-10 15:21:48 +0900

    좀비메쉬 5개 기본코드 추가

---
+++ Source/SAC1/AI/AIZombi/SelfdestructZombi.cpp
@@ -0,0 +1,45 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SelfdestructZombi.h"
+#include "../DefaultAIAnimInstance.h"
+
+ASelfdestructZombi::ASelfdestructZombi()
+{
+
+	PrimaryActorTick.bCanEverTick = true;
+
+	mName = TEXT("selfdestructZombi");
+
+	mBody->SetCapsuleHalfHeight(110.f);
+	mBody->SetCapsuleRadius(33.f);
+
+	static ConstructorHelpers::FObjectFinder<USkeletalMesh>	MeshAsset(TEXT("/Script/Engine.SkeletalMesh'/Game/Zombi/selfdestructZombi/Parasite_L_Starkie.Parasite_L_Starkie'"));
+
+	if (MeshAsset.Succeeded())
+		mMesh->SetSkeletalMesh(MeshAsset.Object);
+
+	mMesh->SetRelativeLocation(FVector(0.0, 0.0, -110.0));
+	mMesh->SetRelativeRotation(FRotator(0.0, -90.0, 0.0));
+
+	static ConstructorHelpers::FClassFinder<UAnimInstance>	AnimAsset(TEXT("/Script/Engine.AnimBlueprint'/Game/Zombi/AB_selfdestructZombi.AB_selfdestructZombi_C'"));
+
+	if (AnimAsset.Succeeded())
+		mMesh->SetAnimInstanceClass(AnimAsset.Class);
+
+}
+
+void ASelfdestructZombi::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ASelfdestructZombi::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+}
+
+void ASelfdestructZombi::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
