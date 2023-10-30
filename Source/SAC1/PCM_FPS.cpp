a1b4a6bd9810365803c54a3f75aaf24ea7291000
ArgoMel <122796999+ArgoMel@users.noreply.github.com>
2023-10-30 18:11:09 +0900

    카메라연습

---
+++ Source/SAC1/PCM_FPS.cpp
@@ -0,0 +1,42 @@
+#include "PCM_FPS.h"
+#include "SAC1Character.h"
+
+APCM_FPS::APCM_FPS()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void APCM_FPS::BeginPlay()
+{
+	Super::BeginPlay();
+	m_Character = Cast<ASAC1Character>(PCOwner->Player);
+	if (IsValid(m_Character))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("dsdd"));
+	}
+}
+
+void APCM_FPS::UpdateViewTarget(FTViewTarget& OutVT, float DeltaTime)
+{
+	Super::UpdateViewTarget(OutVT, DeltaTime);
+}
+
+void APCM_FPS::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	WeaponSway();
+}
+
+FVector APCM_FPS::SwayIdle()
+{
+	return FVector();
+}
+
+FVector APCM_FPS::SwayADS()
+{
+	return FVector();
+}
+
+void APCM_FPS::WeaponSway()
+{
+}
